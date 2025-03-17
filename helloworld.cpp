c32ae4c7cbf4a09960616d634b4ee6ddb135ed0e
willianrezd <willianrez808@gmail.com>
2025-03-16 23:16:27 -0300

    hellow World in c++
    First program of my C++ journey, and it couldn't be different from the classic Hello World

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//hello world, first program in c++
+#include <iostream> //Added a library to enable commands
+    using namespace std;
+        int main(){ 
+            // main  
+            cout << "Hello World\n";
+            // "\n" is a type of command for line breaking
+            cout << "ola mundo" << endl;
+            // "endL" is a other command for line breaking
+ 
+            system ("pause");
+            return 0;
+        }
