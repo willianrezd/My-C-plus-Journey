c43cfb8760b5ea43ea56cbad4389abdb0527ef63
willianrezd <willianrez808@gmail.com>
2025-03-24 20:48:39 -0300

    Definition and multi-variable testing
    EX #3

---
+++ varmuc.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// AULA 5
+
+//AULA DE DECLARAÇÃO DE VARIAVEIS MULTIPLAS E CONSTANTES.
+
+// DECLARANDO UMA CONSTANTE. 
+
+#define pi 3.1415
+
+//TESTE DEFINE
+
+#define barao cout << "barao power breaker \n\n"; 
+
+
+int main(){
+    // DECLARAÇÃO DE VARIAVEIS MULTIPLAS, FORMA CORRETA.
+    //int vida, municao, life; // PODE SE DECLARAR COM QUALQUER TIPO DE VARIAVEL, INT, FLOAT, STRING...ETC.
+    
+    int vida = 3, municao = 150, life = 100;
+    
+    // DECLARAÇÃO DE VARIAVEIS
+    /*int vida;
+    int municao;
+    int life;
+    */
+
+    cout << pi << "\n\n";
+
+    barao
+
+return 0;
+}
+
